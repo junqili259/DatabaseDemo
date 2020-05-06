0a5da119f1c8805253d8f69fabe73748d4d1f459
junqili <junqili259@gmail.com>
2020-05-06 18:17:25 -0400

    added choices for states and shelters

--- forms.py
+++ forms.py
@@ -1,13 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField, IntegerField, RadioField, DateField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms import StringField, TextField, SubmitField, IntegerField, RadioField, DateField, SelectField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+states = [('AL','AL'),('AK','AK'),('AZ','AZ'),('AR','AR'),('CA','CA'),('CO','CO'),('CT','CT'),
+('DE','DE'),('DC','DC'),('FL','FL'),('GA','GA'),('HI','HI'),('ID','ID'),('IL','IL'),('IN','IN'),
+('IA','IA'),('KS','KS'),('KY','KY'),('LA','LA'),('ME','ME'),('MD','MD'),('MA','MA'),('MI','MI'),
+('MN','MN'),('MS','MS'),('MO','MO'),('MT','MT'),('NE','NE'),('NV','NV'),('NH','NH'),('NJ','NJ'),
+('NM','NM'),('NY','NY'),('NC','NC'),('ND','ND'),('OH','OH'),('OK','OK'),('OR','OR'),('PA','PA'),
+('RI','RI'),('SC','SC'),('SD','SD'),('TN','TN'),('TX','TX'),('UT','UT'),('VT','VT'),('VA','VA'),
+('WA','WA'),('WV','WV'),('WI','WI'),('WY','WY'),('PR','PR')]
+
+
+shelter_choices = [(1,'Shelter One'),(2,'Shelter Two'),(3,'Shelter Three'),(4,'Shelter Four'),(5,'Shelter Five'),(6,'Shelter Six'),(7,'Shelter Seven'),(8,'Shelter Eight'),(9,'Shelter Nine'),(10,'Shelter Ten'),
+(11,'Shelter Eleven'),(12,'Shelter Twelve'),(13,'Shelter Thirteen'),(14,'Shelter Fourteen'),(15,'Shelter Fifteen'),(16,'Shelter Sixteen'),(17,'Shelter Seventeen'),(18,'Shelter Eighteen'),(19,'Shelter Nineteen'),(20,'Shelter Twenty')]
 
 class PersonForm(FlaskForm):
     #   Personal Information
     fname = StringField('First Name', validators=[DataRequired()])
     lname = StringField('Last Name', validators=[DataRequired()])
     id_num = IntegerField('ID number', validators=[DataRequired()])
-    sex = RadioField('Sex', choices = [('M','Male'),('F','Female')])
+    sex = RadioField('Sex', choices = [('M','Male'),('F','Female')], validators=[DataRequired()])
     dob = DateField('Date of Birth',format='%Y-%m-%d',validators=[DataRequired()])
     #dob = StringField('Date of Birth', validators=[DataRequired()])
     phone = StringField('Phone Number')
@@ -21,8 +33,15 @@ class PersonForm(FlaskForm):
     v_phone = StringField('Verifier Phone Number', validators=[DataRequired()])
     v_address = StringField('Verifier Address', validators=[DataRequired()])
     v_city = StringField('Verifier City', validators=[DataRequired()])
-    v_state = StringField('Verifier State', validators=[DataRequired()])
+    #v_state = StringField('Verifier State', validators=[DataRequired()])
+    v_state = SelectField('Verifier State', choices=states, validators=[DataRequired()])
     v_zipcode = IntegerField('Verifier Zipcode', validators=[DataRequired()])
+
+
+    #   Shelter History
+    
+    #shelter = SelectField('Shelter Name', choices=shelter_choices,validators=[DataRequired()])
+    
     submit = SubmitField('submit')
 
 
