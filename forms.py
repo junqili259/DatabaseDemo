f42e0085443f5f8ca9848d8902bf7ea14394801b
junqili <junqili259@gmail.com>
2020-05-06 21:01:58 -0400

    Bug fixes

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextField, SubmitField, IntegerField, RadioField, DateField, SelectField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Optional
 
 states = [('AL','AL'),('AK','AK'),('AZ','AZ'),('AR','AR'),('CA','CA'),('CO','CO'),('CT','CT'),
 ('DE','DE'),('DC','DC'),('FL','FL'),('GA','GA'),('HI','HI'),('ID','ID'),('IL','IL'),('IN','IN'),
@@ -21,7 +21,7 @@ class PersonForm(FlaskForm):
     id_num = IntegerField('ID number', validators=[DataRequired()])
     sex = RadioField('Sex', choices = [('M','Male'),('F','Female')], validators=[DataRequired()])
     dob = DateField('Date of Birth, format: yyyy-mm-dd, Example: 2000-01-01',format='%Y-%m-%d',validators=[DataRequired()])
-    phone = StringField('Phone Number')
+    phone = StringField('Phone Number',validators=[Optional()])
     ethnicity = RadioField('Ethnicity',choices=[('American Indian/Alaskan Native','American Indian/Alaskan Native'),('Asian/Pacific Islander','Asian/Pacific Islander'),('Black/African American','Black/African American'),('Hispanic','Hispanic'),('White','White')], validators=[DataRequired()])
     marital_status = RadioField('Marital Status',choices = [('Single','Single'),('Married','Married'),('Widowed','Widowed'),('Divorced','Divorced')], validators=[DataRequired()])
 
@@ -39,8 +39,8 @@ class PersonForm(FlaskForm):
     #   Shelter History
     
     shelter = SelectField('Shelter Name', choices=shelter_choices,validators=[DataRequired()])
-    date_in = DateField('Date In, format: yyyy-mm-dd, Example: 2000-01-01',format='%Y-%m-%d')
-    date_out = DateField('Date Out, format: yyyy-mm-dd, Example: 2000-01-01', format='%Y-%m-%d')
+    date_in = DateField('Date In, format: yyyy-mm-dd, Example: 2000-01-01',format='%Y-%m-%d',validators=[Optional()])
+    date_out = DateField('Date Out, format: yyyy-mm-dd, Example: 2000-01-01', format='%Y-%m-%d',validators=[Optional()])
     
     submit = SubmitField('submit')
 
